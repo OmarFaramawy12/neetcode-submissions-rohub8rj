339f5510f7493674345a6acdc3a4ce0c78340add
Omar Mohamed Faramawy <43156742+OmarFaramawy12@users.noreply.github.com>
2026-04-19 19:55:24 +0200

    Add: python-heapify - submission-1

---
+++ Python Coding Interviews/python-heapify/submission-1.py
@@ -0,0 +1,32 @@
+import heapq
+from typing import List
+
+
+def heapify_strings(strings: List[str]) -> List[str]:
+    # Heap is implemented as List
+    heapq.heapify(strings)
+    return strings
+
+
+
+
+def heapify_integers(integers: List[int]) -> List[int]:
+    heapq.heapify(integers)
+    return integers
+
+
+
+def heap_sort(nums: List[int]) -> List[int]:
+    # heapifying in place
+    heapq.heapify(nums)
+    # sorting in place
+    nums.sort(key = None , reverse = False)
+    return nums
+    
+
+
+
+# do not modify below this line
+print(heapify_strings(["b", "a", "e", "c", "d"]))
+print(heapify_integers([3, 4, 5, 1, 2, 6]))
+print(heap_sort([3, 4, 5, 1, 2, 6]))

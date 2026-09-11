7aaa8cd867cd1e26c953b67d406458342cb0c5a9
Omar Mohamed Faramawy <43156742+OmarFaramawy12@users.noreply.github.com>
2026-09-11 10:52:31 +0300

    Add: maximum-subarray - submission-8

---
+++ Data Structures & Algorithms/maximum-subarray/submission-8.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        '''
+        Main Idea: Avoid Current Negative Sum Running -> wont lead to the greatest summation
+        - curr_sum < 0: -> start new window (Kadan's algorithnm)
+
+        '''
+
+        #left = 0
+        curr_sum = 0
+        max_sum = nums[0]       # Assumption array is non-empty
+
+        for right in range(len(nums)):
+
+
+            '''
+            if curr_sum < 0:
+                curr_sum = 0
+                left = right
+            '''
+            # Same above cod -> write using the max operation
+
+            curr_sum = max(0 , curr_sum)
+            curr_sum += nums[right]
+            max_sum = max(max_sum , curr_sum)
+        return max_sum

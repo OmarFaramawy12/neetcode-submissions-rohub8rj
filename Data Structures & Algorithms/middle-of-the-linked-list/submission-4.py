d6dff836b5bb182f2f270ae057259404ae57a6b5
Omar Mohamed Faramawy <43156742+OmarFaramawy12@users.noreply.github.com>
2026-09-14 18:59:09 +0300

    Add: middle-of-the-linked-list - submission-4

---
+++ Data Structures & Algorithms/middle-of-the-linked-list/submission-4.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        Brute Force Algorithm:
+            - Step-1: traverse the Linked List to count the number of nodes (find the length of list)
+            - Determine whther it's even or odd
+            - Case-1: Even Length: -> middle is index (n/2)
+            - Case-1: Odd length: -> middle is index (n//2) + 1
+            - Time Complexity:
+                a- O(n) First Loop to determine length
+                b- O(n/2) second loop to get middle
+                c- Total -> O(n+ n/2) --> O(3n / 2) -> O(n)
+        MAin Algorithm: Fast and Slow Pointers (Two Pointers Technique)
+
+            - fast and slow pointer starts at the head
+            - Assuming that Fast pointer moves Twice the Speed of SLow Pointer
+            -Main Idea-> 
+                1- Once the Fast Pointer reaches the End of Linked List -> Slow pointer will be at the 
+                middle
+                2- This algorithm works better in two cases (even and Odd length linked lIst) 
+            -Time Complexity:
+                1- Fast Pointer visits (n/2) Nodes: O(n/2)
+                2- Slow Pointer visits (n/2)
+                3- Total -> Number of iterations  * work per iteration
+                    O(n/2) * O(1) - >O(n/2) -> O(n)
+            - Space Complexity: O(1) -> Maintaining only fast and slow pointers
+
+        '''
+        slow , fast = head , head
+
+        while fast and fast.next:
+
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
+                

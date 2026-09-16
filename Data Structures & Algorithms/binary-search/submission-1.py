2dec4cbcef67b46e51ea528b1b169e515cd132a2
Omar Mohamed Faramawy <43156742+OmarFaramawy12@users.noreply.github.com>
2026-09-16 09:52:31 +0300

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        '''
+        MAin Algorithm: TraditionalBinary Search
+
+            - Main-Idea: Elimination half of the search space (Keep Dividing the Search     
+            Space by 2)
+
+            -Time Complexity: -> O(logn)
+            -Space Complexity: -> O(1) maintaining only pointers
+        '''
+
+        left , right = 0 , len(nums) - 1
+        while left <= right:
+            # to prevent Overflow
+            mid = left + (right - left) // 2
+            if target > nums[mid]:
+                # Discard the left portion of Array
+                left = mid + 1
+            elif target < nums[mid]:
+                # Discard the right Portion of Array
+                right = mid - 1
+            else:
+                return mid
+
+        return -1
